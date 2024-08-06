6fc9ce4571f1388e0bdb7c78aa67f083c2c3492d
Ryan Hinds <fossryanh@gmail.com>
2024-08-06 10:50:41 -0300

    Input Manager functional and good to go

--- ABGJ/Assets/Scripts/Managers/GameManager.cs
+++ ABGJ/Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    // This is a temporary enable to allow player movement.
+    protected override void Initialize() { InputManager.Instance.EnableInputType(GameInputType.PlayerControl); }
 }
